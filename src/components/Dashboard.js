e6b7ed7d6a0cd979fdda48dc24decc5767e7ef6f
SsandySANTHOSH <santhoshs4717@gmail.com>
2023-08-19 11:27:38 +0530

    first commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,76 @@
+import React, { useContext, useEffect ,useState} from 'react'
+import { useNavigate } from 'react-router-dom';
+import { LoginContext } from './ContextProvider/Context';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import BackgroundAnimate from './BackgroundAnimate';
+import InputShortener from './InputShortener';
+import LinkResult from './LinkResult';
+
+const Dashboard = () => {
+
+    const { logindata, setLoginData } = useContext(LoginContext);
+
+    const [data, setData] = useState(false);
+
+
+    const history = useNavigate();
+
+    const DashboardValid = async () => {
+        let token = localStorage.getItem("usersdatatoken");
+
+        const res = await fetch("https://backend1-8bj5.onrender.com/validuser", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+            }
+        });
+
+        const data = await res.json();
+
+        if (data.status == 401 || !data) {
+            history("*");
+        } else {
+            console.log("user verify");
+            setLoginData(data)
+            history("/dash");
+        }
+    }
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            DashboardValid();
+            setData(true)
+        }, 2000)
+
+    }, [])
+
+    const [inputValue, setInputValue] = useState("");
+
+    return (
+        <>
+            {
+                data ? <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                    <img src="./man.png" style={{ width: "200px", marginTop: 20 }} alt="" />
+                    <h1>User Email:{logindata ? logindata.ValidUserOne.email : ""}</h1>
+                    <h1>User Name:{logindata ? logindata.ValidUserOne.fname : ""}</h1>
+                    <div className="container">
+      <InputShortener setInputValue={setInputValue} />
+      <BackgroundAnimate />
+      <LinkResult inputValue={inputValue} />
+    </div>
+
+                </div> : <Box sx={{ display: 'flex', justifyContent: "center", alignItems: "center", height: "100vh" }}>
+                    Loading... &nbsp;
+                    <CircularProgress />
+                </Box>
+            }
+
+        </>
+
+    )
+}
+
+export default Dashboard
